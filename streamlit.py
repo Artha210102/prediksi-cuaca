7f07ce0af322c156eed44e65e00a02c667504bd3
Artha210102 <arthanugraha7945@gmail.com>
2025-01-05 22:00:52 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from io import StringIO
 
 # Streamlit App Title
