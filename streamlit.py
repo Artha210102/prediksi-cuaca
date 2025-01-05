ae7f5426894136d23810e1875c51ee8af9629bef
Artha210102 <arthanugraha7945@gmail.com>
2025-01-05 21:57:33 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
