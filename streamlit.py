50aefdf4963330580e4544669448e0cf331cd12b
Artha210102 <arthanugraha7945@gmail.com>
2025-01-05 21:59:41 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from io import StringIO
 
 # Streamlit App Title
 st.title("Weather Classification Analysis")
