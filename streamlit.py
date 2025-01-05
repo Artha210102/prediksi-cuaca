7ea8a69cda23aae2c8fcc7bfde1800932d84654c
Artha210102 <arthanugraha7945@gmail.com>
2025-01-05 21:37:54 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
